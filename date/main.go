c212151010afa86591a9e0966ecdd68c15a3830a
Juan <juan.rios.28@gmail.com>
2019-09-09 19:52:25 -0400

    working tests

--- date/main.go
+++ date/main.go
@@ -1,47 +1,49 @@
 package date
 
 import (
-	"fmt"
 	"time"
 )
 
-func main() {
-	fmt.Println("Welcome from deego/date")
+func sampleTime() time.Time {
+	s := "2019-01-22"
+	layout := "2006-01-02"
+	t, _ := time.Parse(layout, s)
+	return t
 }
 
-var Now = time.Now
-
-// now returns the current system datetime, mainly used for the other datetime functions.
+// now returns the current datetime
 func now() time.Time {
-	return time.Now()
+	t := time.Now()
+	return t
+	// 2019-09-08 00:00:00.619429 -0400 EDT
 }
-
 
 // tomorrow retuns the datetime of the next day from the datetime given
-func tomorrow(t time.Time) time.Time {
+func tomorrow() time.Time {
+	t := sampleTime()
 	// add 1 int day
 	// years, months, days
-	to := t.AddDate(0, 0, 1)
-	return to
+	tomorrow := t.AddDate(0, 0, 1)
+	return tomorrow
 	// today -> 2019-09-08 13:23:52.73977 -0400 EDT
 	// tomorrow -> 2019-09-09 13:23:52.73977 -0400 EDT
 }
 
 // nextWeek returns the datetime of one week from the datetime given
-func nextWeek(t time.Time) time.Time {
+func nextWeek() time.Time {
+	t := sampleTime()
 	// years, months, days
 	// 7 days * the number of weeks we want to add
-	nw := t.AddDate(0, 0, 1)
-	return nw
+	nextWeek := t.AddDate(0, 0, 1)
+	return nextWeek
 	// today -> 2019-09-08 13:23:52.73977 -0400 EDT
 	// next week -> 2019-09-15 13:23:52.73977 -0400 EDT
 }
 
 // isWeekend returns true if a given datetime lands on a weekend, false when it doesn't.
-func isWeekend(s string) bool {
-	dt, _ := time.Parse("2019-09-10 15:04:05", s)
-
-	if (dt.Weekday().String() == "Saturday") || (dt.Weekday().String() == "Sunday") {
+func isWeekend() bool {
+	t := sampleTime()
+	if (t.Weekday().String() == "Saturday") || (t.Weekday().String() == "Sunday") {
 		return true
 	}
 
@@ -49,10 +51,9 @@ func isWeekend(s string) bool {
 }
 
 // isWeekday returns true if a given datetime lands on a weekday, false when it doesn't.
-func isWeekday(s string) bool {
-	dt, _ := time.Parse("2019-09-10 15:04:05", s)
-
-	if (dt.Weekday().String() != "Saturday") || (dt.Weekday().String() != "Sunday") {
+func isWeekday() bool {
+	t := sampleTime()
+	if (t.Weekday().String() != "Saturday") || (t.Weekday().String() != "Sunday") {
 		return true
 	}
 
@@ -60,32 +61,31 @@ func isWeekday(s string) bool {
 }
 
 // getMonthLong returns the full month of a given datetime
-func getMonthLong(s string) string {
-	dt, _ := time.Parse("2019-09-10 15:04:05", s)
+func getMonthLong() string {
+	t := sampleTime()
 	// We want the abbreviated form so lets take the first 3 letters of the string
-	m := dt.Month().String()
+	month := t.Month().String()
 
-	return m
+	return month
 }
 
 // getMonthShort returns the abbreviated month of a given datetime
-func getMonthShort(s string) string {
-	dt, _ := time.Parse("2019-09-10 15:04:05", s)
+func getMonthShort() string {
+	t := sampleTime()
 	// We want the abbreviated form so lets take the first 3 letters of the string
-	m := dt.Month().String()[:3]
+	month := t.Month().String()[:3]
 
-	return m
+	return month
 }
 
 // last returns the datetime from the last day that was given, for example last Thursday
-func last(s time.Weekday) time.Time {
-	t := now()
-
+func last(wd time.Weekday) time.Time {
+	t := sampleTime()
 	// Converts day of week to its int form
-	// Sunday = 0, Saturday = 6
-	day := int(t.Weekday())
-	ds := int(s)
-	diff := ds - day
+	// Sunday = 0, Saturday = 7
+	cd := int(t.Weekday())
+	ld := int(wd)
+	diff := ld - cd
 
 	// subtract a week since the requested day is equal to current day
 	if diff == 0 {
@@ -96,19 +96,18 @@ func last(s time.Weekday) time.Time {
 	if diff < 0 {
 		return t.AddDate(0, 0, diff)
 	}
-	
+
 	return t.AddDate(0, 0, -(7 - diff))
 }
 
 // next returns the dt of the next given day, for example next Thursday
-func next(s time.Weekday) time.Time {
-	t := now()
-
+func next(wd time.Weekday) time.Time {
+	t := sampleTime()
 	// Converts day of week to its int form
-	// Sunday = 0, Saturday = 6
-	day := int(t.Weekday())
-	ds := int(s)
-	diff := ds - day
+	// Sunday = 0, Saturday = 7
+	cd := int(t.Weekday())
+	ld := int(wd)
+	diff := ld - cd
 
 	// add a week since the requested day is equal to current day
 	if diff == 0 {
--- timezone/main.go
+++ timezone/main.go
@@ -1,25 +1,29 @@
 package timezone
 
 import (
-	"fmt"
 	"time"
 )
 
-func main() {
-	fmt.Println("Welcome from deego/timezone")
+func now() time.Time {
+	return time.Now()
+}
+
+func sampleTime() time.Time {
+	s := "2019-01-22"
+	t, _ := time.Parse("2019-01-22", s)
+	return t
 }
 
 // currTimezone returns the timezone from the current datetime.
 func currTimezone() string {
-	t := time.Now()
-	z, _ := t.Zone()
+	z, _ := now().Zone()
 	return z
 	// Current timezone: EST
 }
 
 // getTimezone returns the timezone from a given datetime
-func getTimezone(t time.Time) string {
-	z, _ := t.Zone()
+func getTimezone() string {
+	z, _ := sampleTime().Zone()
 	return z
 	// timezone: EST
 }
