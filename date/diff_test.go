0f19bc99364efb7052d7908f20375b971466f707
Juan Rios <juan.rios.28@gmail.com>
2019-09-10 23:24:38 -0400

    added tests for all date functioins

---
+++ date/diff_test.go
@@ -0,0 +1,39 @@
+package date
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func Test_diff(t *testing.T) {
+	type args struct {
+		t1 time.Time
+		t2 time.Time
+	}
+
+	d1 := randomDate()
+	d2 := randomDate()
+	d3 := randomDate()
+
+	t1 := args{d1, d1.AddDate(0, 0, 1)}
+	t2 := args{d2, d2.AddDate(0, 0, 30)}
+	t3 := args{d3, d3}
+
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"1 day diff", t1, 1},
+		{"2 day diff", t2, 30},
+		{"0 days diff", t3, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			actual := diff(tt.args.t1, tt.args.t2)
+			assert.Equal(t, want, actual, "diff could not be calculated")
+		})
+	}
+}
