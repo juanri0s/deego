d2a30c8228fbb274bfc2126794f990c137cb2db9
Juan Rios <juan.rios.28@gmail.com>
2019-09-23 23:29:13 -0400

    added remaining timezone tests

---
+++ timezone/currtimezone_test.go
@@ -0,0 +1,22 @@
+package timezone
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_currTimezone(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"current timezone test", "EDT"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			want := tt.want
+			actual := currTimezone()
+			assert.Equal(t, want, actual, "unable to determine current timezone from now time")
+		})
+	}
+}
